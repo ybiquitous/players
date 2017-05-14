cd227b53f1c7124f8dde70a4c64a000f99959496
ybiquitous <10koba01@gmail.com>
2017-05-14 17:43:47 +0900

    workaround for build error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const webpack = require('webpack')
 
 module.exports = {
   webpack(config) {
+    // NOTE: workaround for build error (https://github.com/zeit/next.js/issues/1877)
+    if (config.resolve.alias) {
+      /* eslint-disable */
+      delete config.resolve.alias['react']
+      delete config.resolve.alias['react-dom']
+      /* eslint-enable */
+    }
+
     config.plugins.push(new webpack.DefinePlugin({
       'process.env.API_ENDPOINT': JSON.stringify(process.env.API_ENDPOINT),
     }))
